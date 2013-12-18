5a5cc3808f8d0130cca71481f3054cb4575051a3
Ranjith <ran.mandadi@gmail.com>
2013-12-18 20:20:58 +0000

    add liked page

--- config/initializers/simple_captcha.rb
+++ config/initializers/simple_captcha.rb
@@ -16,7 +16,7 @@ SimpleCaptcha.setup do |sc|
   # 'charcoal_grey',
   # 'almost_invisible'
   # 'random'
-  sc.image_style = 'simply_blue'
+  sc.image_style = 'random'
 
   # default: low
   # possible values: 'low', 'medium', 'high', 'random'
