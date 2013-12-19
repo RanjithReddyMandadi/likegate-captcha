41f35bf048f22745a67a825a3a08fb4794278201
Ranjith <ran.mandadi@gmail.com>
2013-12-19 10:30:16 +0000

    code refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,48 +2,30 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   
-def home
-           session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/users/callback')
-                @auth_url = session[:oauth].url_for_oauth_code(:permissions=>"read_stream")         
-                puts session.to_s + "<<< session"
-                
-        session[:signed_request] = session[:oauth].parse_signed_request(params[:signed_request])
-        @a = session[:signed_request]['page']['liked']
+	def home
+           session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL)
+           begin
+            
+	    @auth_url = session[:oauth].url_for_oauth_code(:permissions=>"read_stream")         
+                           
+            session[:signed_request] = session[:oauth].parse_signed_request(params[:signed_request])
+            @a = session[:signed_request]['page']['liked']
         
-        if @a==true
-        
-        redirect_to :new_user
-	end
-          respond_to do |format|
-                         format.html { }
-                 end
-      
+            if @a==true
+             redirect_to :new_user
+	    end
 
-  end
-
-	def callback
-  		if params[:code]
-  		# acknowledge code and get access token from FB
-		  session[:access_token] = session[:oauth].get_access_token(params[:code])
-		end		
-
-		 # auth established, now do a graph call:
-		  
-		@api = Koala::Facebook::API.new(session[:access_token])
-		begin
-			#@graph_data = @api.get_object("/me/statuses", "fields"=>"message")
-		rescue Exception=>ex
-			puts ex.message
-		end
+          rescue Exception=>ex
+	   puts ex.message
+          end
 		
-  
- 		respond_to do |format|
-		 format.html {   }			 
-		end
-		
-	
-	end
-   
+         respond_to do |format|
+         format.html { }
+         end
+
+       end
+
+	   
 
 
  def index
